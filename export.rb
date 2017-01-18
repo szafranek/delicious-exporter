276761e9fa7240084b557953b5e1acfeaa91673a
Krzysztof Szafranek <ebraun@o2.pl>
2017-01-18 04:03:47 +0100

    Add support for downloading private links behind login

--- export.rb
+++ export.rb
@@ -6,7 +6,7 @@ require 'time'
 require 'optparse'
 require 'open-uri'
 require 'tmpdir'
-
+require 'json'
 
 def parse_options
   options = {}
@@ -18,6 +18,10 @@ def parse_options
 
     opts.on("-u", "--username DELICIOUS_USERNAME", "Delicious username") do |u|
       options[:username] = u
+    end
+    
+    opts.on("-p", "--password DELICIOUS_PASSWORD", "Delicious password. If you don't provide it, only public bookmarks will be exported.") do |p|
+      options[:password] = p
     end
   
     opts.on("-o", "--output-file OUTPUT_FILE", "Output file") do |o|
@@ -73,17 +77,44 @@ def element_string(url, el)
   %Q(<DT><A HREF="#{url}" ADD_DATE="#{add_date}" LAST_VISIT="#{add_date}" LAST_MODIFIED="#{add_date}" TAGS="#{tags}">#{title}</A>\n)
 end
 
-def page_count()
-  username = $options[:username]
-  user_page = Nokogiri::HTML(open("https://del.icio.us/#{username}"))
-  link_count = user_page.css(".profileMidpanel h1 span:last-child").text.to_i
-  (link_count / 10).ceil.to_i  
+def login(tmpdir, username, password)
+  cookie_file = "#{tmpdir}/cookie.txt"
+  response_str = `curl -s -c #{cookie_file} -d "username=#{username}" -d "password=#{password}" "https://del.icio.us/login"`
+  response = JSON.parse(response_str)
+  unless response["error_msgs"].empty?
+    puts response["error_msgs"].join("\n")
+    exit
+  end
+  cookie_name, cookie_value = response["session"].to_s.split("=", 2)
+  cookie_time = (Time.now + (60*60*24*365)).to_i
+  # Setting a cookie that del.icio.us sets via JavaScript
+	File.open(cookie_file, "a") { |f|
+    f << "del.icio.us	TRUE	/	FALSE	#{cookie_time}	#{cookie_name}	#{cookie_value}\n"
+  }
+  cookie_file
 end
 
-def download_pages(dir, page_count)
+def cookify_query(query, cookie_file)
+    query << (cookie_file ? " -b '#{cookie_file}'" : "")
+    query
+end
+
+def page_count(tmpdir, cookie_file)
+  username = $options[:username]
+  page = "#{tmpdir}/home.html"
+  query = cookify_query("curl -s 'https://del.icio.us/#{username}' -o '#{page}'", cookie_file)
+  `#{query}`
+  user_page = Nokogiri::HTML(File.open(page))
+  link_count = user_page.css(".profileMidpanel h1 span:last-child").text.to_i
+	pages = (link_count.to_f / 10).ceil.to_i
+	puts "#{link_count} links, #{pages} pages to download"
+  pages
+end
+
+def download_pages(dir, cookie_file, page_count)
   username = $options[:username]
   unless $options[:silent]
-    puts "Downloading #{page_count} pages to a temporary directory #{dir}"
+    puts "Downloading #{page_count} pages to a temporary directory..."
   end
   if page_count > 1
     pages = "[1-#{page_count}]"
@@ -92,7 +123,7 @@ def download_pages(dir, page_count)
     pages = "1"
     current_page = "1"
   end
-  query = %Q{curl -s 'https://del.icio.us/#{username}?&page=#{pages}' -o "#{dir}/page-#{current_page}.html"}
+  query = cookify_query("curl -s 'https://del.icio.us/#{username}?&page=#{pages}' -o '#{dir}/page-#{current_page}.html'", cookie_file)
   `#{query}`
 end
 
@@ -103,8 +134,9 @@ end
 def bookmarks_string
   items_string = ""
   tmpdir = Dir.mktmpdir
-  page_count = page_count()
-  download_pages(tmpdir, page_count)
+  cookie_file = $options[:password] ? login(tmpdir, $options[:username], $options[:password]) : nil
+  page_count = page_count(tmpdir, cookie_file)
+  download_pages(tmpdir, cookie_file, page_count)
   for n in 1..page_count
   	page = Nokogiri::HTML(open("#{tmpdir}/page-#{n}.html"))
   	elements = page.css(".articleThumbBlockOuter")
